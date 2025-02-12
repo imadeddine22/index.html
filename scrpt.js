a101f3a2c3354f2b32f608f08d24c6f67835764c
imadeddine22 <ieddine008@gmail.com>
2025-02-12 12:22:08 -1100

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,10 @@
+voyageur = prompt('entrez le nom du voyageur:');
+distance = Number(prompt('entrez  de la distance:'));
+pointa = prompt('entrez la city de depart:');
+pointb = prompt('entrez la city de destination:');
+
+// هاذي مليحا بسك الدولار يخليك تبدل الفاريابل هذاك كيما تحب
+document.write(`le voyageur ${voyageur} a traversé une distance de ${distance} KM de point A : ${pointa} vers point B : ${pointb}`);
+
+
+
